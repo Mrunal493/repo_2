d10144ab650b943325aed3aee3c63e5e94b068aa
Mrunal493 <aashjagtap257@gmail.com>
2025-05-08 21:43:10 +0530

    commited

--- HelloStudent.java
+++ HelloStudent.java
@@ -2,5 +2,6 @@ public class HelloStudent{
    public static void main(String[] args){
       System.out.println("Hello my name is Jerry");
       System.out.println("WElcome to CDAC");
+      System.out.println("Welcome to DITISS");
    }
 }
