428746574d7ec06ab2b2c07d24c377229062cf59
Mrunal493 <aashjagtap257@gmail.com>
2025-05-08 20:47:19 +0530

    Modified commit

--- HelloStudent.java
+++ HelloStudent.java
@@ -2,6 +2,6 @@ public class HelloStudent {
   public static void main(String[] args){
      System.out.println("Hello, my name is Jerry");
      System.out.println("Welcome to CDAC ACTS");
-     System.out.println("Welcome to DITISS");
+     System.out.println("Welcome to Ditiss");
    }
 }
